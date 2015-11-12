acb1a9952d3c9a7d9038b45f8d15c5bbf0da5605
aeoleader <aeoleader@gmail.com>
2015-11-11 19:02:31 -0500

    Initial Commit

---
+++ CS300PROJECT3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS300PROJECT3
+//
+//  Created by Xiangyu on 11/11/15.
+//  Copyright © 2015 Xiangyu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
