5b0059fded13a170e611199e77d9d3801628ee90
aeoleader <aeoleader@gmail.com>
2015-11-16 23:12:49 -0500

    first merge without minion

--- CS300PROJECT3/main.cpp
+++ CS300PROJECT3/main.cpp
@@ -20,6 +20,7 @@
 #include <cstdlib>
 #include <iostream>
 #include "tgaClass.h"
+#include <vector>
 
 using namespace std;
 GLUquadricObj *obj;
@@ -52,19 +53,8 @@ GLfloat g_zoom = 120;
 
 GLUquadricObj *qobj;         // Pointer for quadric objects.
 
-// Settings for minion
 
-// Rotation for arms
-static GLfloat armVert = 0.0, strechX = 0.0, strechY = 0.0, armSpeed = 1.0;
-bool maxWave = false, waveHand = false;
-static GLint fastFlag = 0;
 
-float runningAngle = 0;
-bool running = false;
-int flag = 1;
-float speed = 2;
-int dist = 0;
-int face = 0;
 
 // Bounding box
 struct BoundingBox {
@@ -77,6 +67,7 @@ struct BoundingBox {
     
     BoundingBox(int _left=0, int _right=0, int _top=0, int _bottom=0, int _front=0, int _back=0): left(_left), right(_right), top(_top), bottom(_bottom), front(_front), back(_back) {}
 };
+
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 /************************** Texture Mapping Operations ******************************************/
@@ -169,433 +160,6 @@ void init(void)
     glShadeModel (GL_SMOOTH);
     glEnable(GL_DEPTH_TEST);            // turn on the depth buffer
 }
-
-//////////////////////////////////////////////////////////////////////////////////////////////////
-/***************************************** Draw Minion ******************************************/
-// draw shoe tip
-void drawShoeTip()
-{
-    int density = 250;
-    glColor3f(0.0,0.0,0.0);
-    glBegin(GL_POINTS);
-    for (int i = -density; i < density; i++)
-    {
-        double buf = i * (1.5/density);
-        double z = -buf*buf + 2.25;
-        for (int j = 0; j< density; j++)
-        {
-            double x = j * (buf/density);
-            double y = sqrt(buf*buf - x*x);
-            glVertex3f(x, y, z);
-        }
-    }
-    glEnd();
-}
-
-// draw shoe body
-void drawShoeBody(float l, float w)
-{
-    glTranslatef(0, 0, -0.5*l);
-    int density = 250;
-    glColor3f(0.0,0.0,0.0);
-    glBegin(GL_POINTS);
-    for (int i = -density; i < density; i++)
-    {
-        for (int j = -density; j< density; j++)
-        {
-            double x = i * (w/density);
-            double z = j * (0.5*l/density);
-            double y = sqrt(2.25 - x*x);
-            glVertex3f(x, y, z);
-        }
-    }
-    glEnd();
-}
-
-// draw feet
-void minionFeet()
-{
-    
-    glPushMatrix();
-    glRotatef(runningAngle, 1.0, 0.0, 0.0);
-    //Right leg
-    glPushMatrix();
-    glTranslatef(3.0, -16.0, 0.0);
-    glRotatef(90, 1.0, 0.0, 0.0);
-    gluCylinder(qobj, 3, 2, 6, 30, 30);
-    glPopMatrix();
-    
-    glPushMatrix();
-    glColor3f(0.9, 0.5, 0.0);
-    glTranslatef(3, -20, 0.0);
-    glRotatef(90, 1.0, 0.0, 0.0);
-    gluCylinder(qobj, 1.5, 1.4, 3.2, 30, 30);
-    glPopMatrix();
-    
-    glPushMatrix();
-    glColor3f(1.0, 1.0, 1.0);
-    glTranslatef(3, -23.3, 0.0);
-    glTranslatef(0, -1, 1.5);
-    drawShoeTip();
-    drawShoeBody(3, 2);
-    glPopMatrix();
-    glPopMatrix();
-    
-    //Left leg
-    glPushMatrix();
-    glRotatef(-runningAngle, 1.0, 0.0, 0.0);
-    
-    glPushMatrix();
-    glTranslatef(-3.0, -16.0, 0.0);
-    glColor3f(0.0,0.0,0.61);
-    glRotatef(90, 1.0, 0.0, 0.0);
-    gluCylinder(qobj, 3, 2, 6, 30, 30);
-    glPopMatrix();
-    
-    glPushMatrix();
-    glColor3f(0.9, 0.5, 0.0);
-    glTranslatef(-3, -20, 0.0);
-    glRotatef(90, 1.0, 0.0, 0.0);
-    gluCylinder(qobj, 1.5, 1.4, 3.2, 30, 30);
-    glPopMatrix();
-    
-    glPushMatrix();
-    glColor3f(1.0, 1.0, 1.0);
-    glTranslatef(-3, -23.3, 0.0);
-    glTranslatef(0, -1, 1.5);
-    drawShoeTip();
-    drawShoeBody(3, 2);
-    glPopMatrix();
-    glPopMatrix();
-    
-}
-
-// draw body
-void minionBody()
-{
-    
-    glPushMatrix();
-    glColor3f(0.9,0.5,0.0);
-    glRotatef(90.0,1.0,0.0,0.0);
-    gluCylinder(qobj,8.0,8.0,12,50,50); //middle body
-    glPopMatrix();
-    
-    glPushMatrix();
-    glColor3f(0.9,0.5,0.0);
-    glutSolidSphere(8.0,20,20); //head sphere
-    glPopMatrix();
-    
-    glPushMatrix();
-    glColor3f(0.0,0.0,0.61);
-    glTranslatef(0.0,-12,0.0);
-    glutSolidSphere(8.0,20,20); //bottom sphere
-    glPopMatrix();
-    
-    //goggles strap
-    glPushMatrix();
-    glColor3f(0.36,0.25,0.20);
-    glRotatef(90,1.0,0.0,0.0);
-    glutSolidTorus (1.0,7.1,30,120);
-    glPopMatrix();
-    
-    //right goggles
-    glPushMatrix();
-    glColor3f(0.90,0.91,0.98);
-    glTranslatef(3.0,0.0,6.0);
-    for (float i = 2.0; i < 3; i = i + 0.001)
-        gluCylinder(qobj,i,i,2.5,50,50);
-    glPopMatrix();
-    
-    //left goggles
-    glPushMatrix();
-    glColor3f(0.90,0.91,0.98);
-    glTranslatef(-3.0,0.0,6.0);
-    for (float i = 2.0; i < 3; i = i + 0.001)
-        gluCylinder(qobj,i,i,2.5,50,50);
-    glPopMatrix();
-    
-    //right eye
-    glPushMatrix();
-    glColor3f(1.0,1.0,1.0);
-    glTranslatef(2.6,0.0,6.0);
-    glutSolidSphere(2.5,20,20); //white eyeball
-    glPopMatrix();
-    
-    glPushMatrix();
-    glColor3f(0.35,0.16,0.14);
-    glTranslatef(2.8,0.0,8.0);
-    glutSolidSphere(0.9,20,20); //brown part
-    glPopMatrix();
-    
-    glPushMatrix();
-    glColor3f(0.0,0.0,0.0);
-    glTranslatef(2.8,0.0,8.4);
-    glutSolidSphere(0.6,20,20); //tiny black part
-    glPopMatrix();
-    
-    //left eyeball
-    glPushMatrix();
-    glColor3f(1.0,1.0,1.0);
-    glTranslatef(-2.6,0.0,6.0);
-    glutSolidSphere(2.5,20,20); //white eyeball
-    glPopMatrix();
-    
-    glPushMatrix();
-    glColor3f(0.35,0.16,0.14);
-    glTranslatef(-2.8,0.0,8.0);
-    glutSolidSphere(0.9,20,20); //brown part
-    glPopMatrix();
-    
-    glPushMatrix();
-    glColor3f(0.0,0.0,0.0);
-    glTranslatef(-2.8,0.0,8.4);
-    glutSolidSphere(0.6,20,20); //tiny black part
-    glPopMatrix();
-    
-    //smile
-    glPushMatrix();
-    glTranslatef(0.0, -5.5, 0.0);
-    glPointSize(2.0);
-    glColor3f(1.0,0.0,0.0);
-    glBegin(GL_POINTS);
-    for (int i = 0; i < 1000; i++)
-    {
-        double x = i*0.003;
-        double y = 0.1*x*x;
-        double z = sqrt(64 - x*x);
-        glVertex3f(x, y, z);
-        glVertex3f(-x, y, z);
-    }
-    glEnd();
-    glPopMatrix();
-    
-    //clothing
-    glPushMatrix();
-    glColor3f(0.0,0.0,1.0);
-    
-    //middle part clothing
-    glBegin(GL_POLYGON);
-    glVertex3f(-1.2,-7.5,8.0);
-    glVertex3f(-1.2,-12.0,8.0);
-    glVertex3f(1.2,-12.0,8.0);
-    glVertex3f(1.2,-7.5,8.0);
-    glEnd();
-    
-    //grey patch
-    glColor3f(1.0,1.0,1.0);
-    glBegin(GL_POLYGON);
-    glVertex3f(1.2,-8.5,8.1);
-    glVertex3f(-1.2,-8.5,8.1);
-    glVertex3f(-1.2,-10.5,8.1);
-    glVertex3f(1.2,-10.5,8.1);
-    glEnd();
-    
-    //left side clothing
-    glColor3f(0.0,0.0,1.0);
-    glBegin(GL_POLYGON);
-    glVertex3f(-1.6,-7.5,7.9);
-    glVertex3f(-1.6,-12.1,7.9);
-    glVertex3f(-1.2,-12.1,7.9);
-    glVertex3f(-1.2,-7.5,7.9);
-    glEnd();
-    
-    glBegin(GL_POLYGON);
-    glVertex3f(-2.0,-7.5,7.8);
-    glVertex3f(-2.0,-12.1,7.8);
-    glVertex3f(-1.6,-12.1,7.9);
-    glVertex3f(-1.6,-7.5,7.9);
-    glEnd();
-    
-    glBegin(GL_POLYGON);
-    glVertex3f(-2.4,-7.5,7.7);
-    glVertex3f(-2.4,-12.1,7.7);
-    glVertex3f(-2.0,-12.1,7.8);
-    glVertex3f(-2.0,-7.5,7.8);
-    glEnd();
-    
-    glBegin(GL_POLYGON);
-    glVertex3f(-3.0,-7.5,7.6);
-    glVertex3f(-3.0,-12.1,7.6);
-    glVertex3f(-2.4,-12.1,7.7);
-    glVertex3f(-2.4,-7.5,7.7);
-    glEnd();
-    
-    glBegin(GL_POLYGON);
-    glVertex3f(-4.0,-7.5,7.2);
-    glVertex3f(-4.0,-12.1,7.2);
-    glVertex3f(-3.0,-12.1,7.5);
-    glVertex3f(-3.0,-7.5,7.5);
-    glEnd();
-    
-    //right side clothing
-    glBegin(GL_POLYGON);
-    glVertex3f(1.6,-7.5,7.9);
-    glVertex3f(1.6,-12.1,7.9);
-    glVertex3f(1.2,-12.1,7.9);
-    glVertex3f(1.2,-7.5,7.9);
-    glEnd();
-    
-    glBegin(GL_POLYGON);
-    glVertex3f(2.0,-7.5,7.8);
-    glVertex3f(2.0,-12.1,7.8);
-    glVertex3f(1.6,-12.1,7.9);
-    glVertex3f(1.6,-7.5,7.9);
-    glEnd();
-    
-    glBegin(GL_POLYGON);
-    glVertex3f(2.4,-7.5,7.7);
-    glVertex3f(2.4,-12.1,7.7);
-    glVertex3f(2.0,-12.1,7.8);
-    glVertex3f(2.0,-7.5,7.8);
-    glEnd();
-    
-    glBegin(GL_POLYGON);
-    glVertex3f(3.0,-7.5,7.6);
-    glVertex3f(3.0,-12.1,7.6);
-    glVertex3f(2.4,-12.1,7.7);
-    glVertex3f(2.4,-7.5,7.7);
-    glEnd();
-    
-    glBegin(GL_POLYGON);
-    glVertex3f(4.0,-7.5,7.2);
-    glVertex3f(4.0,-12.1,7.2);
-    glVertex3f(3.0,-12.1,7.5);
-    glVertex3f(3.0,-7.5,7.5);
-    glEnd();
-    glPopMatrix();
-    
-    //shoulder straps
-    glPushMatrix();
-    glColor3f(0.0,0.0,1.0);
-    glTranslatef(0.0,-7.5,0.0);
-    glRotatef(90,1.0,0.0,0.0);
-    glutSolidTorus (1.0,7.3,30,120);
-    glPopMatrix();
-}
-
-// draw arm
-void minionArm(bool isLeft, bool handUp)
-{
-    glColor3f(0.9,0.5,0.0);
-    GLfloat radi = 1.5, fingerRadi = 0.7;
-    
-    int sign = isLeft ? 1 : -1; // left/right parameters offset
-    
-    // upper arm
-    glPushMatrix();
-    if (handUp && isLeft)
-    {
-        glRotatef(-180, 1.0, 0.0, 0.0);
-        glTranslatef(strechX, strechY, 0.0);
-        glRotatef(armVert, 0.0, 0.0, 1.0);
-    }
-    else if (handUp && !isLeft)
-    {
-        glRotatef(-180, 1.0, 0.0, 0.0);
-        glTranslatef(-strechX, strechY, 0.0);
-        glRotatef(-armVert, 0.0, 0.0, 1.0);
-    }
-    
-    glPushMatrix();
-    glRotatef(sign * -25, 0.0, 0.0, 1.0);
-    glRotatef(-90, 1.0, 0.0, 0.0);
-    gluCylinder(qobj, radi, radi, 4, 30, 5);
-    gluSphere(qobj, radi, 30, 30);
-    glPopMatrix();
-    
-    glPushMatrix();
-    glTranslatef(sign * -0.44, -1.8, 0.0);
-    glRotatef(sign * -12, 0.0, 0.0, 1.0);
-    glRotatef(-90, 1.0, 0.0, 0.0);
-    gluCylinder(qobj, radi, radi, 2.0, 30, 5);
-    gluSphere(qobj, radi, 30, 30);
-    glPopMatrix();
-    
-    //fore arm
-    glPushMatrix();
-    glTranslatef(sign * -0.4, -3.0, 0.0);
-    glRotatef(sign * 1, 0.0, 0.0, 1.0);
-    glRotatef(-90, 1.0, 0.0, 0.0);
-    gluCylinder(qobj, radi, radi, 2.0, 30, 5);
-    gluSphere(qobj, radi, 30, 30);
-    glPopMatrix();
-    
-    // glove (hand)
-    glColor3f(0.0, 0.0, 0.0);
-    glPushMatrix();
-    glTranslatef(sign * -0.6, -5.2, 0.0);
-    glRotatef(sign * 2, 0.0, 0.0, 1.0);
-    glRotatef(-90, 1.0, 0.0, 0.0);
-    gluCylinder(qobj, radi + 0.1, radi + 0.5, 1.6, 30, 5);
-    gluSphere(qobj, radi + 0.1, 30, 30);
-    glPopMatrix();
-    
-    // fingers
-    glPushMatrix();
-    glTranslatef(sign * -0.6, -7.0, 1.0);
-    glRotatef(sign * 2, 0.0, 0.0, 1.0);
-    glRotatef(-80, 1.0, 0.0, 0.0);
-    gluCylinder(qobj, fingerRadi, fingerRadi, 1.4, 30, 5);
-    gluSphere(qobj, fingerRadi, 30, 30);
-    glPopMatrix();
-    
-    glPushMatrix();
-    glTranslatef(sign * 0.7, -6.6, 0.8);
-    glRotatef(sign * 20, 0.0, 0.0, 1.0);
-    glRotatef(-80, 1.0, 0.0, 0.0);
-    gluCylinder(qobj, fingerRadi, fingerRadi, 1.4, 30, 5);
-    gluSphere(qobj, fingerRadi, 30, 30);
-    glPopMatrix();
-    
-    glPushMatrix();
-    glTranslatef(sign * -1.8, -6.6, 0.8);
-    glRotatef(sign * -20, 0.0, 0.0, 1.0);
-    glRotatef(-80, 1.0, 0.0, 0.0);
-    gluCylinder(qobj, fingerRadi, fingerRadi, 1.4, 30, 5);
-    gluSphere(qobj, fingerRadi, 30, 30);
-    glPopMatrix();
-    
-    glPopMatrix();
-}
-
-void drawMinion(void)
-{
-    glPushMatrix();
-    qobj = gluNewQuadric();
-    glScalef(0.1, 0.1, 0.1);
-    glTranslatef(0.0, 30.0, 0.0);
-    glRotatef(180, 0.0, 1.0, 0.0);
-    minionBody();
-    minionFeet();
-    
-    // left arm
-    glPushMatrix();
-    if (waveHand)
-    {
-        glTranslatef(-8.0, -7.0, 0.0);
-    }
-    else
-        glTranslatef(-7.8, -11.0, 0.0);
-    minionArm(true, waveHand);
-    glPopMatrix();
-    
-    // right arm
-    glPushMatrix();
-    if (waveHand && fastFlag)
-    {
-        glTranslatef(8.0, -7.0, 0.0);
-        minionArm(false, waveHand);
-    }
-    else
-    {
-        glTranslatef(7.8, -11.0, 0.0);
-        minionArm(false, false);
-    }
-    glPopMatrix();
-    glPopMatrix();
-}
-/***************************************** Draw Minion ******************************************/
-//////////////////////////////////////////////////////////////////////////////////////////////////
 
 void drawBackground(void)
 {
@@ -765,20 +329,6 @@ void display(void)
     glLoadIdentity();
     
     camera();
-//    /******** Lighting Settings ********/
-//    glEnable(GL_LIGHTING);
-//    glEnable(GL_LIGHT0);
-//    
-//    glMaterialfv(GL_FRONT, GL_SPECULAR, no_mat);
-//    glMaterialf(GL_FRONT, GL_SHININESS, 100);
-//    glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
-//    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-//    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-//    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-//    
-//    glEnable(GL_COLOR_MATERIAL);
-//    /******** End Lighting ********/
-
 
     //    /******** Lighting Settings ********/
         glEnable(GL_LIGHTING);
@@ -807,12 +357,7 @@ void display(void)
     drawFloating(9.0);
     glPopMatrix();
     
-//    glPushMatrix();
-//    glTranslatef(0.0, -2.0, 118.0);
-//    drawFloating(3.0);
-//    drawMinion();
-//    glPopMatrix();
-//    
+  
     glPushMatrix();
     glTranslatef(0.0,0.85,100.0);
     glutSolidTeapot(0.25);
@@ -948,19 +493,9 @@ void keyboard(unsigned char key, int x, int y)
             exit(0);
             break;
         case 'w':
-//            yrotrad = (yrot / 180 * 3.141592654f);
-//            xrotrad = (xrot / 180 * 3.141592654f);
-//            xpos += float(0.1*sin(yrotrad)) ;
-//            zpos -= float(0.1*cos(yrotrad)) ;
-//            ypos -= float(0.1*sin(xrotrad)) ;
             forwarding = true;
             break;
         case 's':
-//            yrotrad = (yrot / 180 * 3.141592654f);
-//            xrotrad = (xrot / 180 * 3.141592654f);
-//            xpos -= float(0.1*sin(yrotrad));
-//            zpos += float(0.1*cos(yrotrad)) ;
-//            ypos += float(0.1*sin(xrotrad));
             backwarding = true;
             break;
         case 'd':
@@ -999,31 +534,6 @@ void keyup(unsigned char key, int x, int y)
     }
 }
 
-// arrow keys that are used to control the rotation of the object
-void arrowkeys( int key, int x, int y )
-{
-    switch (key) {
-        case GLUT_KEY_UP:							// If we hit the UP arrow key
-            g_zoom -= 1;
-            glutPostRedisplay();
-            break;
-        case GLUT_KEY_DOWN:							// If we hit the DOWN arrow key
-            g_zoom += 1;
-            glutPostRedisplay();
-            break;
-        case GLUT_KEY_LEFT:							// If we hit the LEFT arrow key
-            g_rotateX -= 2;
-            glutPostRedisplay();
-            break;
-        case GLUT_KEY_RIGHT:						// If we hit the RIGHT arrow key
-            g_rotateX += 2;
-            glutPostRedisplay();
-            break;
-        default:
-            break;
-    }
-    glutPostRedisplay();
-}
 
 
 // main function
@@ -1037,7 +547,6 @@ int main(int argc, char **argv)
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
     glutKeyboardUpFunc(keyup);
-    glutSpecialFunc(arrowkeys);
     glutIdleFunc(idle);
     glutPassiveMotionFunc(mouseMovement);
     init();
